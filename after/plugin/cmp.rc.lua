cf47577aea9d0f16b56f500ed76be3821254c51c
CoreDX1 <chrismaquis@gmail.com>
2022-08-18 22:37:15 -0300

    Tabnine Icon

--- after/plugin/cmp.rc.lua
+++ after/plugin/cmp.rc.lua
@@ -5,7 +5,15 @@ local lspkind = require 'lspkind'
 require 'cmp'.setup {
   sources = {
     { name = 'cmp_tabnine' },
+    { name = 'path' }
   },
+}
+local source_mapping = {
+	buffer = "[Buffer]",
+	nvim_lsp = "[LSP]",
+	nvim_lua = "[Lua]",
+	cmp_tabnine = "[TN]",
+	path = "[Path]",
 }
 
 cmp.setup({
@@ -27,12 +35,24 @@ cmp.setup({
     }),
   }),
   sources = cmp.config.sources({
+    { name = 'cmp_tabnine' },
     { name = 'nvim_lsp' },
     { name = 'buffer' },
-    { name = 'cmp_tabnine' },
+    { name = 'path' }
   }),
   formatting = {
-    format = lspkind.cmp_format({ with_text = false, maxwidth = 50 })
+    format = function(entry, vim_item)
+			vim_item.kind = lspkind.presets.default[vim_item.kind]
+			local menu = source_mapping[entry.source.name]
+			if entry.source.name == 'cmp_tabnine' then
+				if entry.completion_item.data ~= nil and entry.completion_item.data.detail ~= nil then
+					menu = entry.completion_item.data.detail .. ' ' .. menu
+				end
+				vim_item.kind = ''
+			end
+			vim_item.menu = menu
+			return vim_item
+		end
   }
 })
 
