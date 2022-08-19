efcb1af8b73ae64aaf109c1f1076ff35a4a909db
CoreDX1 <chrismaquis@gmail.com>
2022-08-18 22:24:50 -0300

    cmp

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -63,6 +63,11 @@ nvim_lsp.cssls.setup {
     on_attach = on_attach,
     capabilities = capabilities
 }
+nvim_lsp.bashls.setup{
+    on_attach = on_attach,
+    capabilities = capabilities
+}
+
 
 nvim_lsp.html.setup {
     on_attach = on_attach,
