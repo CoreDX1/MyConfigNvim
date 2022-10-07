d0987a5ba81019c80a3222eebab0fdb097f83a5b
CoreDX1 <chrismaquis@gmail.com>
2022-10-07 14:26:52 -0300

    :heavy_minus_sign: OneDark
    Tema Deprecado

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -92,7 +92,6 @@ packer.startup(function(use)
     use {
         "folke/which-key.nvim"
     }
-    use { "olimorris/onedarkpro.nvim" }
     use { "mfussenegger/nvim-jdtls", ft = "java" }
     use { "akinsho/toggleterm.nvim", tag = 'v2.*' }
 end)
