0ea1c0a7f4c1b5c82d9c0fc5eb063383f0c746a5
CoreDX1 <chrismaquis@gmail.com>
2022-11-12 10:56:16 -0300

    :camera_flash: Remove RequireS(Smart)

--- lua/maps.lua
+++ lua/maps.lua
@@ -1,16 +1,19 @@
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 local map = vim.api.nvim_set_keymap
 local set = vim.keymap.set
-map('n', '<C-h>', '<C-w>h', {noremap = true, silent = false})
-map('n', '<C-l>', '<C-w>l', {noremap = true, silent = false})
-map('n', '<C-j>', '<C-w>j', {noremap = true, silent = false})
-map('n', '<C-k>', '<C-w>k', {noremap = true, silent = false})
 
-map('i', 'jk', '<ESC>', {noremap = true, silent = false})
-map('i', 'kj', '<ESC>', {noremap = true, silent = false})
+map("n", "<C-h>", "<C-w>h", { noremap = true, silent = false })
+map("n", "<C-l>", "<C-w>l", { noremap = true, silent = false })
+map("n", "<C-j>", "<C-w>j", { noremap = true, silent = false })
+map("n", "<C-k>", "<C-w>k", { noremap = true, silent = false })
 
-map('n', '<leader>e', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
+map("i", "jk", "<ESC>", { noremap = true, silent = false })
+map("i", "kj", "<ESC>", { noremap = true, silent = false })
+
+map("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+
 --Htop
+--
 set("n", "f", "<cmd>HopChar1CurrentLineAC<CR>")
 set("n", "F", "<cmd>HopChar1CurrentLineBC<CR>")
 set("n", "s", "<cmd>HopChar2AC<CR>")
@@ -19,15 +22,20 @@ set("n", "<leader>s", "<cmd>HopLineStartAC<CR>")
 set("n", "<leader>S", "<cmd>HopLineStartBC<CR>")
 
 --Buffer
+--
 set("n", "<S-l>", "<cmd>BufferLineCycleNext<CR>")
 set("n", "<S-h>", "<cmd>BufferLineCyclePrev<CR>")
 
---smart
-set('n', '<C-h>', require('smart-splits').move_cursor_left)
-set('n', '<C-j>', require('smart-splits').move_cursor_down)
-set('n', '<C-k>', require('smart-splits').move_cursor_up)
-set('n', '<C-l>', require('smart-splits').move_cursor_right)
-set('n', '<A-h>', require('smart-splits').resize_left)
-set('n', '<A-j>', require('smart-splits').resize_down)
-set('n', '<A-k>', require('smart-splits').resize_up)
-set('n', '<A-l>', require('smart-splits').resize_right)
+--smart Cursor Moved
+
+set("n", "<C-h>", "<cmd>SmartCursorMoveLeft<CR>")
+set("n", "<C-j>", "<cmd>SmartCursorMoveDown<CR>")
+set("n", "<C-k>", "<cmd>SmartCursorMoveUp<CR>")
+set("n", "<C-l>", "<cmd>SmartCursorMoveRight<CR>")
+
+-- Smart Resize Moved
+
+set("n", "<A-h>", "<cmd>SmartResizeLeft<CR>")
+set("n", "<A-j>", "<cmd>SmartCursorMoveDown<CR>")
+set("n", "<A-k>", "<cmd>SmartCursorMoveUp<CR>")
+set("n", "<A-l>", "<cmd>SmartResizeRight<CR>")
